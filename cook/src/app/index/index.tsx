8fe0b2367e21f20ae7888e3ef1a56376d6d6037d
Bruno Becoski <brunobecoski@gmail.com>
2024-03-05 21:39:19 -0300

    Cria o projeto Cook

---
+++ cook/src/app/index/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+import { styles } from "./styles";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>React Native na Prática</Text>
+    </View>
+  )
+}
---
+++ cook/src/app/index/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'deeppink',
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    color: 'white',
+  },
+});
